1fa354d5670a0d65691fcee8436b18461b63166c
Akshay-kumar-sah <akki19082001@gmail.com>
2023-07-25 09:47:08 +0530

    Basic Setup

---
+++ Controllers/BookController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MVCProject.Controllers
+{
+    // [ApiController]
+    // [Route("{[controller]}")]
+    public class BookController : Controller
+    {
+        public readonly ILogger<BookController> _logger;
+
+        public BookController(ILogger<BookController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+
+        public IActionResult Security()
+        {
+             return View();
+        }
+    }
+}
